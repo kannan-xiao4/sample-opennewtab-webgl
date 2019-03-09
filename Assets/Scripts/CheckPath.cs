11c497210aee171e378ad11b09ba7a32ae84c6c9
TakashiKannan <wingrakhi@live.jp>
2019-03-10 00:13:46 +0900

    first commit

---
+++ Assets/Scripts/CheckPath.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckPath : MonoBehaviour
+{
+    [SerializeField] private Text pathText;
+
+    private void Start()
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine($"streamingAssetsPath : {Application.streamingAssetsPath}");
+        builder.AppendLine($"persistentDataPath : {Application.persistentDataPath}");
+        builder.AppendLine($"temporaryCachePath : {Application.temporaryCachePath}");
+
+        pathText.text = builder.ToString();
+    }
+}
---
+++ Assets/Scripts/OpenNewTab.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenNewTab : MonoBehaviour
+{
+    [SerializeField] private InputField urlInput;
+    [SerializeField] private Button openUrlButton;
+    
+#if UNITY_WEBGL
+    //Javascriptの呼び出し
+    [DllImport("__Internal")]
+    private static extern void OpenNewTabEventOn(string url);
+    [DllImport("__Internal")]
+    private static extern void OpenNewTabEventOff();
+#endif
+    
+    private void Start()
+    {
+        var clickedEvent = new Button.ButtonClickedEvent();
+        clickedEvent.AddListener(() => OpenUrl(urlInput.text));
+        
+        openUrlButton.onClick = clickedEvent;
+    }
+
+    private void OpenUrl(string url)
+    {
+#if UNITY_WEBGL
+        OpenNewTabEventOn(url);
+#else
+        Application.OpenURL(url);
+#endif  
+    }
+}
